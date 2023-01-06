388556dbaa7f4603f912c36aff23f2b08f68cf64
root <root@ch69-8.maas>
2023-01-06 09:57:46 +0000

    bug fix

--- SecurityManagement/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SecuritymanagementConfig(AppConfig):
-    name = 'SecurityManagement'
--- SecurityManagement/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class RegisterForm(forms.Form):
-    name = forms.CharField(max_length=150, required=True)
-    password = forms.CharField(max_length=128, required=True)
